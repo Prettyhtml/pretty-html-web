7edc3451bce5721c77d10ad3d5fa242cb2985248
Mesut Koca <imesutkoca@gmail.com>
2018-10-02 05:38:26 +0300

    disable eslint rule

--- src/workers/index.js
+++ src/workers/index.js
@@ -5,6 +5,7 @@ let prettyhtmlWorker = null;
 
 export function getWorkers() {
   if (prettyhtmlWorker === null) {
+    // eslint-disable-next-line no-console
     console.log("[pretty-html-worker]: Worker hasn't initialized yet.");
   }
   return {
