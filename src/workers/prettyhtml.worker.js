73ba11aef9e2903829eff03c6fd67e1d9d4b6cdc
starptech <deusdustin@gmail.com>
2018-10-20 11:36:21 +0200

    add todo

--- src/workers/prettyhtml.worker.js
+++ src/workers/prettyhtml.worker.js
@@ -3,5 +3,6 @@ importScripts("prettyhtml.browserified.min.js");
 
 export function run(code, opts) {
   const vFile = prettyhtml(code, opts);
+  // TODO: Show vfile messages e.g warnings in the debug section
   return vFile.contents;
 }
