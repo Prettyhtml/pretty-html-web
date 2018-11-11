c05374686cf380f5136f468e827b6778465bd7f1
Mesut Koca <imesutkoca@gmail.com>
2018-11-11 23:22:46 +0300

    fix mobile logo margin

--- src/App.vue
+++ src/App.vue
@@ -152,7 +152,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"><g fill="currentColor"><path v-if="showSidebar" d="M20.05 11H5.91l1.3-1.29a1 1 0 0 0-1.42-1.42l-3 3a1 1 0 0 0 0 1.42l3 3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42L5.91 13h14.14a1 1 0 0 0 .95-.95V12a1 1 0 0 0-.95-1z"/><rect x="3" y="11" width="18" height="2" rx=".95" ry=".95"/><rect x="3" y="17" width="18" height="2" rx=".95" ry=".95"/><rect x="3" y="5" width="18" height="2" rx=".95" ry=".95"/></g></svg>
         </button>
         <div v-if="!showSidebar" class="logo-mobile ml-6 flex items-center">
-          <a href="#" class="logo w-32 mt-px">
+          <a href="#" class="logo w-32 -my-1 ">
             <img src="./assets/logo.png" class="" alt="PrettyHtml" />
           </a>
         </div>
